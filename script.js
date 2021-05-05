e9765b012abcf0f1334de8a122422fc8729a34f1
marikaklein <72083493+marikaklein@users.noreply.github.com>
2021-05-05 00:41:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+console.log("Hello, Airtable");
+
+var Airtable = require("airtable");
+
+var base = new Airtable({ apiKey: "key36DMlmYm8Ica1w" }).base(
+  "appTz1eD3FGxYdsgU"
+);
+ 
+base("books").select({
+	view: "year"
+}).eachPage(gotPageOfBooks, gotAllBooks);
+
+var books = [];
+
+function gotPageOfBooks(records, fetchNextPage){
+console.log("gotPageOfBooks()");
+books.push(...records);
+fetchNextPage();
+}
+
+function gotAllBooks(err){
+console.log("gotAllBooks()");
+
+if (err) {
+	console.log("error loading books");
+	console.error(err);
+	return;
+}
+
+consoleLogBooks();
+showBooks();
+}
+
+function consoleLogBooks() {
+	console.log("consoleLogBooks()");
+	books.forEach((book) => {
+		console.log("Book:", book);
+	});
+}
+
+function showBooks() {
+	console.log("showBooks()");
+	books.forEach((book) => {
+
+		let bookContainer = document.createElement("div");
+		bookContainer.classList.add("book-container");
+		document.querySelector(".container").append(bookContainer);
+	
+		let bookTitle = document.createElement("h1");
+		bookTitle.classList.add("book-title");
+		bookTitle.innerText = book.fields.title;
+		bookContainer.append(bookTitle);
+
+		let bookAuthor = document.createElement("h2");
+		bookAuthor.classList.add("book-author");
+		bookAuthor.innerText = book.fields.author;
+		bookContainer.append(bookAuthor);
+
+		let bookYear = document.createElement("h3");
+		bookYear.classList.add("book-year");
+		bookYear.innerText = book.fields.year;
+		bookContainer.append(bookYear);
+
+		var bookCover = document.createElement("img");
+		bookCover.classList.add("book-cover");
+		bookCover.src = book.fields.cover[0].url;
+		bookContainer.append(bookCover);
+
+		bookContainer.addEventListener("click", function() {
+			bookYear.classList.toggle("active");
+			bookCover.classList.toggle("active");
+		})
+
+		var bookYears = book.fields.year;
+		bookYears.forEach(function(year) {
+			bookContainer.classList.add(year)
+		})
+
+		var filterOne = document.querySelector('.2020');
+		filterOne.addEventListener("click", function() {
+
+			if (bookContainer.classList.contains("2020")) {
+				bookContainer.style.display = "block";
+			} else {
+				bookContainer.style.display = "none";
+			}
+		})
+
+		var filterTwo = document.querySelector('.2021');
+		filterTwo.addEventListener("click", function() {
+
+			if (bookContainer.classList.contains("2021")) {
+				bookContainer.style.display = "block";
+			} else {
+				bookContainer.style.display = "none";
+			}
+		})
+
+		var filterReset = document.querySelector('.js-reset')
+		filterReset.addEventListener("click", function(){
+				bookContainer.style.display = "block";
+			})
+
+	});
+}
